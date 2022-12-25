5a474938b8de9763d8820f38168df6a013eaa0d5
Shidik Saragih <shidiksaragih@gmail.com>
2022-12-25 19:53:09 +0700

    Contoh Library Sederhana Composer

---
+++ src/Customer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MajasStudio\Belajar;
+
+class Customer
+{
+    public function __construct(private string $name)
+    {
+        
+    }
+
+    public function sayHello(string $name): string
+    {
+        return "Hello $name, My Name is $this->name";
+    }
+
+}
